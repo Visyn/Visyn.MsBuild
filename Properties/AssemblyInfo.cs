9cd07bf0ac9a465a50dd9daa30f5cc8f8c7125c8
randlee <github@randlee.com>
2016-03-19 16:17:29 -0700

    moving to visyn namespace Updated copyright date updated assembly title/info/version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BuildTools")]
+[assembly: AssemblyTitle("Visyn.Build")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BuildTools")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Visyn.Build")]
+[assembly: AssemblyCopyright("Copyright ©  2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.242")]
-[assembly: AssemblyFileVersion("1.0.0.242")]
+[assembly: AssemblyVersion("1.1.2.246")]
+[assembly: AssemblyFileVersion("1.1.2.246")]
